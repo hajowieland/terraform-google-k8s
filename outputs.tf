05970f66dc494b7d37fc099b78395efa303257cd
hajowieland <mail@wieland.tech>
2019-07-07 23:38:00 +0200

    add latest available k8s master version on GKE

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@
 output "kubeconfig_path_gke" {
   value = "${local_file.kubeconfiggke.0.filename}"
 }
+
+output "latest_k8s_master_version" {
+  value = data.google_container_engine_versions.current.*.latest_master_version
+}
